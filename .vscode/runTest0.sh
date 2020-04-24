68ff019acb13ee633fafe5adf7a0741a7a5b3169
wzaylor <will.zaylor@gmail.com>
2020-04-24 11:24:34 -0400

    Added the notes section, and updated the test case.

--- .vscode/runTest0.sh
+++ .vscode/runTest0.sh
@@ -1,4 +1,4 @@
 cd test
-source /opt/intel/compilers_and_libraries_2019/linux/bin/compilervars.sh intel64
+source /opt/intel/bin/compilervars.sh intel64
 
-/home/will/Projects/MCLS_CSU/febio_source_2.9/FEBioSource2.9/build/bin/febio2.lnx64 BeamBending.feb
+/home/will/Projects/MCLS_CSU/febio_source_2.9/FEBioSource2.9/build/bin/febio2.lnx64 UniaxialLoad.feb
